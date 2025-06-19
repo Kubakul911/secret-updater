3368c580268db60e2986e5d8ce3c1ddb7e45b99a
Kubakul911 <djjx8626@gmail.com>
2025-06-19 10:41:06 +0300

    Delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-def test_function():
-    import telebot
-    bot = telebot.TeleBot("8147830929:AAGXHZWYuev7guc2pPYHWLKx56yua8-G5BM")
-    bot.send_message(1440476292, "✅ Тестовая функция сработала!")
