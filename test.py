84257e961b3948b0aecf67c4a72919a0f02bf783
Kubakul911 <djjx8626@gmail.com>
2025-06-19 10:35:00 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,4 @@
+def test_function():
+    import telebot
+    bot = telebot.TeleBot("8147830929:AAGXHZWYuev7guc2pPYHWLKx56yua8-G5BM")
+    bot.send_message(1440476292, "✅ Тестовая функция сработала!")
